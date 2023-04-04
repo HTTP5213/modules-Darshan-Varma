ff0105e862466d3cfeba81896ecd396fd58f2474
Darshan Varma <darshanvarma030597@gmail.com>
2023-04-03 21:25:18 -0400

    lab2-es6

---
+++ public/my-module.js
@@ -0,0 +1,6 @@
+
+import { theShire, buckland } from './another-module.js';
+
+const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
+
+export {statement,theShire,};
